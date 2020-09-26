acee8bbfa7849b62a0bff29075f25de62d15e748
CYPAMR99 <cmedinar1@miumg.edu.gt>
2020-09-26 12:38:20 -0600

    Prototipo No Funcional Corregido Fase 3

---
+++ ProyectoCLC_ModuloSCM/CapaVista/CapaVista/Login/frmLogin.designer.cs
@@ -0,0 +1,165 @@
+﻿namespace Prototipo_No_Funcional.Login
+{
+    partial class frmLogin
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmLogin));
+            this.pnlLogo = new System.Windows.Forms.Panel();
+            this.pnlPrincipal = new System.Windows.Forms.Panel();
+            this.pbxLogo = new System.Windows.Forms.PictureBox();
+            this.lblUsuario = new System.Windows.Forms.Label();
+            this.lblClave = new System.Windows.Forms.Label();
+            this.txtUsuario = new System.Windows.Forms.TextBox();
+            this.txtClave = new System.Windows.Forms.TextBox();
+            this.btnIngresar = new System.Windows.Forms.Button();
+            this.pnlLogo.SuspendLayout();
+            this.pnlPrincipal.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxLogo)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pnlLogo
+            // 
+            this.pnlLogo.Controls.Add(this.pbxLogo);
+            this.pnlLogo.Dock = System.Windows.Forms.DockStyle.Left;
+            this.pnlLogo.Location = new System.Drawing.Point(0, 0);
+            this.pnlLogo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pnlLogo.Name = "pnlLogo";
+            this.pnlLogo.Size = new System.Drawing.Size(380, 283);
+            this.pnlLogo.TabIndex = 0;
+            // 
+            // pnlPrincipal
+            // 
+            this.pnlPrincipal.BackColor = System.Drawing.Color.White;
+            this.pnlPrincipal.Controls.Add(this.btnIngresar);
+            this.pnlPrincipal.Controls.Add(this.txtClave);
+            this.pnlPrincipal.Controls.Add(this.txtUsuario);
+            this.pnlPrincipal.Controls.Add(this.lblClave);
+            this.pnlPrincipal.Controls.Add(this.lblUsuario);
+            this.pnlPrincipal.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pnlPrincipal.Location = new System.Drawing.Point(380, 0);
+            this.pnlPrincipal.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pnlPrincipal.Name = "pnlPrincipal";
+            this.pnlPrincipal.Size = new System.Drawing.Size(382, 283);
+            this.pnlPrincipal.TabIndex = 1;
+            // 
+            // pbxLogo
+            // 
+            this.pbxLogo.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.pbxLogo.Image = ((System.Drawing.Image)(resources.GetObject("pbxLogo.Image")));
+            this.pbxLogo.Location = new System.Drawing.Point(0, 0);
+            this.pbxLogo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.pbxLogo.Name = "pbxLogo";
+            this.pbxLogo.Size = new System.Drawing.Size(380, 283);
+            this.pbxLogo.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pbxLogo.TabIndex = 0;
+            this.pbxLogo.TabStop = false;
+            // 
+            // lblUsuario
+            // 
+            this.lblUsuario.AutoSize = true;
+            this.lblUsuario.Location = new System.Drawing.Point(26, 79);
+            this.lblUsuario.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblUsuario.Name = "lblUsuario";
+            this.lblUsuario.Size = new System.Drawing.Size(70, 20);
+            this.lblUsuario.TabIndex = 0;
+            this.lblUsuario.Text = "Usuario";
+            // 
+            // lblClave
+            // 
+            this.lblClave.AutoSize = true;
+            this.lblClave.Location = new System.Drawing.Point(30, 158);
+            this.lblClave.Name = "lblClave";
+            this.lblClave.Size = new System.Drawing.Size(100, 20);
+            this.lblClave.TabIndex = 1;
+            this.lblClave.Text = "Contraseña";
+            // 
+            // txtUsuario
+            // 
+            this.txtUsuario.Location = new System.Drawing.Point(158, 79);
+            this.txtUsuario.Name = "txtUsuario";
+            this.txtUsuario.Size = new System.Drawing.Size(184, 27);
+            this.txtUsuario.TabIndex = 2;
+            // 
+            // txtClave
+            // 
+            this.txtClave.Location = new System.Drawing.Point(158, 155);
+            this.txtClave.Name = "txtClave";
+            this.txtClave.Size = new System.Drawing.Size(184, 27);
+            this.txtClave.TabIndex = 3;
+            // 
+            // btnIngresar
+            // 
+            this.btnIngresar.BackColor = System.Drawing.Color.LimeGreen;
+            this.btnIngresar.FlatAppearance.BorderSize = 0;
+            this.btnIngresar.FlatAppearance.MouseDownBackColor = System.Drawing.Color.GreenYellow;
+            this.btnIngresar.FlatAppearance.MouseOverBackColor = System.Drawing.Color.ForestGreen;
+            this.btnIngresar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnIngresar.Location = new System.Drawing.Point(130, 221);
+            this.btnIngresar.Name = "btnIngresar";
+            this.btnIngresar.Size = new System.Drawing.Size(121, 36);
+            this.btnIngresar.TabIndex = 4;
+            this.btnIngresar.Text = "Ingresar";
+            this.btnIngresar.UseVisualStyleBackColor = false;
+            this.btnIngresar.Click += new System.EventHandler(this.btnIngresar_Click);
+            // 
+            // frmLogin
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(762, 283);
+            this.Controls.Add(this.pnlPrincipal);
+            this.Controls.Add(this.pnlLogo);
+            this.Font = new System.Drawing.Font("Rockwell", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.MaximizeBox = false;
+            this.MinimizeBox = false;
+            this.Name = "frmLogin";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Inicio de Sesión";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.frmLogin_FormClosing);
+            this.pnlLogo.ResumeLayout(false);
+            this.pnlPrincipal.ResumeLayout(false);
+            this.pnlPrincipal.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pbxLogo)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel pnlLogo;
+        private System.Windows.Forms.PictureBox pbxLogo;
+        private System.Windows.Forms.Panel pnlPrincipal;
+        private System.Windows.Forms.Button btnIngresar;
+        private System.Windows.Forms.TextBox txtClave;
+        private System.Windows.Forms.TextBox txtUsuario;
+        private System.Windows.Forms.Label lblClave;
+        private System.Windows.Forms.Label lblUsuario;
+    }
+}
---
+++ ProyectoCLC_ModuloSCM/CapaVista/CapaVista/MDI/frmMDI.cs
@@ -0,0 +1,62 @@
+﻿using Prototipo_No_Funcional.Mantenimientos.Compras;
+using Prototipo_No_Funcional.Mantenimientos.Inventarios;
+using Prototipo_No_Funcional.Mantenimientos.Proveedores;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Prototipo_No_Funcional.MDI
+{
+    public partial class frmMDI : Form
+    {
+        public frmMDI()
+        {
+            InitializeComponent();
+        }
+
+        private void inventariosToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmInventario_Principal inventario = new frmInventario_Principal();
+            inventario.Show();
+        }
+
+        private void comprasToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmCompras_Principal compras = new frmCompras_Principal();
+            compras.Show();
+        }
+
+        private void proveedoresToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            frmProveedor_Principal proveedor = new frmProveedor_Principal();
+            proveedor.Show();
+        }
+
+        private void tmrHoraFecha_Tick(object sender, EventArgs e)
+        {
+            lblHora.Text = DateTime.Now.ToLongTimeString();
+            lblFecha.Text = DateTime.Now.ToLongDateString();
+        }
+
+        private void frmMDI_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            DialogResult drResultadoMensaje;
+            drResultadoMensaje = MessageBox.Show("¿Realmente desea salir?", "", MessageBoxButtons.YesNo, MessageBoxIcon.Exclamation);
+            if (drResultadoMensaje == DialogResult.Yes)
+            {
+                this.Dispose();
+                this.Close();
+            }
+            else
+            {
+                e.Cancel = true;
+            }
+        }
+    }
+}
